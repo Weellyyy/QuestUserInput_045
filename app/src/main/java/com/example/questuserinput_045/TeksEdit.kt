a716be94b067e39e5ccf3d7a758a8481409fd757
Weellyyy <wildanprtm2304@gmail.com>
2025-10-31 14:49:38 +0700

    mendeklarasikan variabel untuk mengingat nilai yang dimasukan oleh keyboard

--- app/src/main/java/com/example/questuserinput_045/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_045/TeksEdit.kt
@@ -1,2 +1,17 @@
 package com.example.questuserinput_045
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FormDataDiri(modifier: Modifier)
+{
+    //variable untuk menginagat value dari keyboard
+    var textNama by remember {mutableStateOf("")}
+    var textAlamat by remember {mutableStateOf("")}
+    var TextJK by remember {mutableStateOf("")}
+}

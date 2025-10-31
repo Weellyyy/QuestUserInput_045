e9fb59601a8a1f7d1b58c69809f4eef273f58478
Weellyyy <wildanprtm2304@gmail.com>
2025-10-31 15:10:12 +0700

    pembuatan input field untuk nama lengkap dengan tata letak vertikal

--- app/src/main/java/com/example/questuserinput_045/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_045/TeksEdit.kt
@@ -1,11 +1,23 @@
 package com.example.questuserinput_045
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FormDataDiri(modifier: Modifier)
@@ -13,7 +25,7 @@ fun FormDataDiri(modifier: Modifier)
     //variable untuk menginagat value dari keyboard
     var textNama by remember {mutableStateOf("")}
     var textAlamat by remember {mutableStateOf("")}
-    var TextJK by remember {mutableStateOf("")}
+    var textJK by remember {mutableStateOf("")}
 
     //variabel untuk menyimppan data yang diperoleh oleh komponen ui
     var nama by remember { mutableStateOf("")}
@@ -22,5 +34,19 @@ fun FormDataDiri(modifier: Modifier)
 
     val gender:List<String> = listOf("Laki-laki","Perempuan")
 
+    Column(modifier = Modifier.padding(top = 50.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        OutlinedTextField(
+            value = textNama,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.width(250.dp),
+            label = { Text(text = "Nama Lengkap") },
+            onValueChange = {
+                textNama = it
+            }
+        )
 
+    }
 }

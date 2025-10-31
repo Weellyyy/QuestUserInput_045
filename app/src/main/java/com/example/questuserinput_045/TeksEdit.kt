f774e4ae76385d1abd1152ca5daec9e2a8015db1
Weellyyy <wildanprtm2304@gmail.com>
2025-10-31 15:26:07 +0700

    Menambahkan tombol yang menyimpan data nama, alamat, dan jenis kelamin saat diklik.

--- app/src/main/java/com/example/questuserinput_045/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_045/TeksEdit.kt
@@ -3,9 +3,11 @@ package com.example.questuserinput_045
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -20,6 +22,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -83,5 +86,16 @@ fun FormDataDiri(modifier: Modifier)
             thickness = dimensionResource(R.dimen.divider_tipis),
             color = Color.DarkGray
         )
+        Button(
+            modifier = Modifier.fillMaxWidth(1f),
+            enabled = textAlamat.isNotEmpty(),
+            onClick = {
+                nama = textNama
+                alamat = textAlamat
+                jenis = textJK
+            }
+        ){
+            Text(stringResource(R.string.submit))
+        }
     }
 }

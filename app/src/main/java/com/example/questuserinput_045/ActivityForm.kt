8936bdee78e9c34f3e0419ec978c9e5b2db0b007
Weellyyy <wildanprtm2304@gmail.com>
2025-11-01 21:16:17 +0700

    membuat textfield untuk RW

--- app/src/main/java/com/example/questuserinput_045/ActivityForm.kt
+++ app/src/main/java/com/example/questuserinput_045/ActivityForm.kt
@@ -176,6 +176,27 @@ fun ActivityForm(modifier: Modifier= Modifier)
                         cursorColor = Color.White
                     )
                 )
+                TextField(
+                    value = textRw,
+                    onValueChange = {textRw = it},
+                    label = {Text("RW")},
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(bottom = 10.dp),
+                    shape = RoundedCornerShape(16.dp),
+                    singleLine = true,
+                    colors = TextFieldDefaults.colors(
+                        focusedContainerColor = Color.White.copy(alpha = 0.3f),
+                        unfocusedContainerColor = Color.White.copy(alpha = 0.3f),
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        focusedLabelColor = Color.White,
+                        unfocusedLabelColor = Color.White.copy(alpha = 0.7f),
+                        focusedTextColor = Color.White,
+                        unfocusedTextColor = Color.White,
+                        cursorColor = Color.White
+                    )
+                )
             }
         }
     }

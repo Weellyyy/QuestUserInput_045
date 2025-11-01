bca1db4c91562a3185d6101d6b3fa21324332286
Weellyyy <wildanprtm2304@gmail.com>
2025-11-01 20:47:40 +0700

    membuat glass container

--- app/src/main/java/com/example/questuserinput_045/ActivityForm.kt
+++ app/src/main/java/com/example/questuserinput_045/ActivityForm.kt
@@ -1,8 +1,13 @@
 package com.example.questuserinput_045
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
@@ -12,7 +17,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -47,7 +56,20 @@ fun ActivityForm(modifier: Modifier= Modifier)
     ){
         Image(
             painter = painterResource(R.drawable.greenbg),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxSize(),
+            contentScale = ContentScale.Crop
         )
+        Column (
+            modifier = modifier
+                .fillMaxWidth(0.9f)
+                .fillMaxWidth(0.9f)
+                .clip(RoundedCornerShape(32.dp))
+                .background(Color.White.copy(alpha = 0.2f))
+                .padding(horizontal = 24.dp, vertical = 24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){  }
     }
 }
 

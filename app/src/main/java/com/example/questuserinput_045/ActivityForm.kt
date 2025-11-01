94886b77c7168b911a415b2489a4136a7dd37b5a
Weellyyy <wildanprtm2304@gmail.com>
2025-11-01 16:22:47 +0700

    membuat variable untuk menyimpan input dari keyboard

--- app/src/main/java/com/example/questuserinput_045/ActivityForm.kt
+++ app/src/main/java/com/example/questuserinput_045/ActivityForm.kt
@@ -1,6 +1,24 @@
 package com.example.questuserinput_045
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 
 @OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ActivityForm(modifier: Modifier= Modifier)
+{
+    var textNamaLengkap by remember { mutableStateOf("") }
+    var textKotaAsal by remember { mutableStateOf("") }
+    var textTanggalLahir by remember { mutableStateOf("") }
+    var textRt by remember { mutableStateOf("") }
+    var textRw by remember { mutableStateOf("") }
+    var textUmur by remember { mutableStateOf("") }
+    var textJenisKelamin by remember { mutableStateOf("") }
+    var isSetujuChecked by remember { mutableStateOf(false) }
+}
 

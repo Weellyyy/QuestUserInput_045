2dd45d1e6945ea5f21216764d1a4deb343c74e62
Weellyyy <wildanprtm2304@gmail.com>
2025-11-01 20:42:06 +0700

    membuat box dan memanggil gambar sebagai background

--- app/src/main/java/com/example/questuserinput_045/ActivityForm.kt
+++ app/src/main/java/com/example/questuserinput_045/ActivityForm.kt
@@ -1,5 +1,8 @@
 package com.example.questuserinput_045
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
@@ -7,7 +10,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -34,5 +39,15 @@ fun ActivityForm(modifier: Modifier= Modifier)
 
     val showDatePicker = remember { mutableStateOf(false) }
     val datePickerState = rememberDatePickerState()
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+        Image(
+            painter = painterResource(R.drawable.greenbg),
+        )
+    }
 }
 
